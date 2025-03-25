08509540b87fdb62b2021bcdd774b1ee21f87270
Michael <micsun@student.unimelb.edu.au>
2025-03-25 15:30:24 +1100

    Added attributes and methods to student

--- app/src/main/java/com/unimelb/swen30006/workshops/Student.java
+++ app/src/main/java/com/unimelb/swen30006/workshops/Student.java
@@ -1,4 +1,13 @@
 package com.unimelb.swen30006.workshops;
 
 public class Student extends Person{
+    private String email;
+    private String studentID;
+
+    public void sendEmailMsg(String message){
+    }
+
+    public float totalGrade() {
+        return (float) 100;
+    }
 }
