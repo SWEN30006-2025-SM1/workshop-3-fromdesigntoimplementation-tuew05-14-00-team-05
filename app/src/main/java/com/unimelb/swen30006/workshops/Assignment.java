d9e806c110b2b03062f5bbf71332a81d32dc2e8a
Amy <axsun@student.unimelb.edu.au>
2025-03-25 15:40:23 +1100

    Implemented Subject and Assignment class

--- app/src/main/java/com/unimelb/swen30006/workshops/Assignment.java
+++ app/src/main/java/com/unimelb/swen30006/workshops/Assignment.java
@@ -1,4 +1,11 @@
 package com.unimelb.swen30006.workshops;
 
+import java.util.Date;
+
 public class Assignment {
+    private String description;
+    private Date dueDate;
+    private Integer maxAttempts;
+    private String name;
+    private Subject subject;
 }
--- app/src/main/java/com/unimelb/swen30006/workshops/Subject.java
+++ app/src/main/java/com/unimelb/swen30006/workshops/Subject.java
@@ -1,4 +1,10 @@
 package com.unimelb.swen30006.workshops;
+import java.util.ArrayList;
 
 public class Subject {
+    private String name;
+    private String subjectCode;
+    private ArrayList<Assignment> assignments;
+    private ArrayList<Staff> staff;
+
 }
